e2ed06ce47555337bfd15aaeaec27e35e7dd84c6
Alan Smith <commit@alanwsmith.com>
2025-12-25 17:51:02 -0600

    added the details state opener

--- docs/bits/default.js
+++ docs/bits/default.js
@@ -233,3 +233,80 @@ export class ImageUpdater {
     }
   }
 }
+
+export class DetailsOpener {
+  #state;
+
+  bittyReady() {
+    this.loadState();
+    this.setTagState();
+    this.watchTags();
+  }
+
+  numberOfTags() {
+    return document.querySelectorAll("details").length;
+  }
+
+  setTagState() {
+    document.querySelectorAll("details").forEach((tag, tagIndex) => {
+      if (
+        this.#state.openTags.has(tagIndex)
+      ) {
+        tag.open = true;
+      } else {
+        tag.open = false;
+      }
+    });
+  }
+
+  loadState() {
+    const storage = localStorage.getItem(this.storageName());
+    this.#state = {
+      numberOfTags: this.numberOfTags(),
+      openTags: new Set(),
+    };
+    document.querySelectorAll("details").forEach((tag, tagIndex) => {
+      if (tag.open === true) {
+        this.#state.openTags.add(tagIndex);
+      }
+    });
+    if (storage !== null) {
+      const data = JSON.parse(storage).state;
+      if (this.numberOfTags() === data.numberOfTags) {
+        this.#state = {
+          numberOfTags: data.numberOfTags,
+          openTags: new Set(data.openTags),
+        };
+      }
+    }
+  }
+
+  saveState() {
+    localStorage.setItem(
+      this.storageName(),
+      JSON.stringify({
+        "state": {
+          numberOfTags: this.#state.numberOfTags,
+          openTags: [...this.#state.openTags],
+        },
+      }),
+    );
+  }
+
+  storageName() {
+    return `details-state-${window.location.pathname}`;
+  }
+
+  watchTags() {
+    document.querySelectorAll("details").forEach((tag, tagIndex) => {
+      tag.addEventListener("toggle", (event) => {
+        if (tag.open) {
+          this.#state.openTags.add(tagIndex);
+        } else {
+          this.#state.openTags.delete(tagIndex);
+        }
+        this.saveState();
+      });
+    });
+  }
+}
